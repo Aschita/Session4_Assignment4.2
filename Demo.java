cbad998eeee9e4690f3ea6007499c53bac5f91f2
Aschita <aschita02@gmail.com>
2017-06-24 12:37:02 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,14 @@
+package assignment_4b;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		    Maruti myMaruti = new Maruti();  
+	        myMaruti.setColor("RED");  
+	        myMaruti.setMaxSpeed(180);  
+	        myMaruti.carInfo();  
+	        myMaruti.MarutiStartDemo();
+	}
+
+}
---
+++ Engine.java
@@ -0,0 +1,10 @@
+package assignment_4b;
+
+public class Engine { 
+	    public void start(){  
+	        System.out.println("Engine Started:");  
+	    }  
+	    public void stop(){  
+	        System.out.println("Engine Stopped:");  
+	    }  
+}
